6d29dfff8951cbd4973b5201fb6a9f914969b7c4
Amadeusz <amadeusz.filipek@gmail.com>
2020-01-13 18:25:11 +0100

    debug

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -28,7 +28,7 @@ AWS_S3_REGION_NAME = 'eu-central-1' #change to your region
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
